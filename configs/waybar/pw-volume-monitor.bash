b9170b369068f29d299a95c1aec834d1ded009da
ksawlii <ksawery.blaszczak@proton.me>
2025-06-24 15:06:01 +0200

    hypr: waybar: fixup
    Signed-off-by: ksawlii <ksawery.blaszczak@proton.me>

--- configs/waybar/pw-volume-monitor.bash
+++ configs/waybar/pw-volume-monitor.bash
@@ -12,10 +12,10 @@ snore()
 DELAY=0.5
 
 SET2=(
-    $'\uf6a9  '
-    $'\uf026  '
-    $'\uf027  ' 
-    $'\uf028  ' 
+    $'\uf6a9   '
+    $'\uf026   '
+    $'\uf027   ' 
+    $'\uf028   ' 
 )
 ICON=("${SET2[@]}")
 
@@ -26,20 +26,20 @@ while snore $DELAY ; do
         if [[ "$LINE" =~ ^Volume:.([0-9]+)\.([0-9]{2})(([[:blank:]]\[MUTED\])?)$ ]] ; then
 
             if [[ -n "$OUT" ]] ; then
-                OUT+=" "
+                OUT+="  "
             fi
             if [[ -n "${BASH_REMATCH[3]}" ]] ; then
                 OUT+="${ICON[0]}Mute   "
             else
                 volume=$(( 10#${BASH_REMATCH[1]}${BASH_REMATCH[2]} ))
                 if [[ $volume -gt 50 ]]; then
-                    OUT+="${ICON[3]}$volume%%   "
+                    OUT+="${ICON[3]}$volume%%    "
                 elif [[ $volume -gt 25 ]]; then
-                    OUT+="${ICON[2]}$volume%%   "
+                    OUT+="${ICON[2]}$volume%%    "
                 elif [[ $volume -gt 0 ]]; then
-                    OUT+="${ICON[1]}$volume%%   "
+                    OUT+="${ICON[1]}$volume%%    "
                 else
-                    OUT+="${ICON[1]}---  "
+                    OUT+="${ICON[1]}---   "
                 fi
             fi
         else
