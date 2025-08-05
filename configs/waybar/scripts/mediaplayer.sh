f51c94072897f276ebc600313eec405792c4fdda
Ksawlii <maja.blaszczak@proton.me>
2025-08-05 22:53:51 +0200

    hypr: waybar: Use double brackets
    Signed-off-by: Ksawlii <maja.blaszczak@proton.me>

--- configs/waybar/scripts/mediaplayer.sh
+++ configs/waybar/scripts/mediaplayer.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 player_status=$(playerctl --player=spotify status)
-if [ "$player_status" = "Playing" ]; then
+if [[ "$player_status" = "Playing" ]]; then
     echo "   $(playerctl --player=spotify metadata artist) - $(playerctl --player=spotify metadata title)"
-elif [ "$player_status" = "Paused" ]; then
+elif [[ "$player_status" = "Paused" ]]; then
     echo "   $(playerctl --player=spotify metadata artist) - $(playerctl --player=spotify metadata title)"
 else
     echo ""
--- configs/waybar/scripts/pw-volume-monitor.bash
+++ configs/waybar/scripts/pw-volume-monitor.bash
@@ -6,7 +6,7 @@ snore()
 {
     local IFS
 
-    [ -n "${_snore_fd:-}" ] || exec {_snore_fd}<> <(:)
+    [[ -n "${_snore_fd:-}" ]] || exec {_snore_fd}<> <(:)
     read ${1:+-t "$1"} -u $_snore_fd || :
 }
 
@@ -25,18 +25,18 @@ while snore $DELAY; do
     INPUT="$(wpctl get-volume @DEFAULT_AUDIO_SINK@)"
     while read LINE; do
         if [[ "$LINE" =~ ^Volume:.([0-9]+)\.([0-9]{2})(([[:blank:]]\[MUTED\])?)$ ]]; then
-            if [ -n "$OUT" ]; then
+            if [[ -n "$OUT" ]]; then
                 OUT+="  "
             fi
-            if [ -n "${BASH_REMATCH[3]}" ]; then
+            if [[ -n "${BASH_REMATCH[3]}" ]]; then
                 OUT+="${ICON[0]}Mute  "
             else
                 volume="$(( 10#${BASH_REMATCH[1]}${BASH_REMATCH[2]} ))"
-                if [ "$volume" -gt 50 ]; then
+                if [[ "$volume" -gt 50 ]]; then
                     OUT+="${ICON[3]} $volume%%   "
-                elif [ "$volume" -gt 25 ]; then
+                elif [[ "$volume" -gt 25 ]]; then
                     OUT+="${ICON[2]} $volume%%   "
-                elif [ "$volume" -gt 0 ]; then
+                elif [[ "$volume" -gt 0 ]]; then
                     OUT+="${ICON[1]} $volume%%   "
                 else
                     OUT+="${ICON[1]}---   "
@@ -48,7 +48,7 @@ while snore $DELAY; do
         fi
     done <<< "$INPUT"
 
-    if [ -n "$OUT" ]; then
+    if [[ -n "$OUT" ]]; then
        printf "$OUT\n"
     fi
 done
